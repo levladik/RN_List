0b6f426bfe6b5c2f43ecf92f3d0558dbc265d929
Levladik <vladislavtyshenko@yandex.ru>
2024-05-10 16:44:44 +0400

    create list of pokemons

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import pokemonList from './data.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => {
+            return (
+              <View style={styles.card} key={pokemon.id}>
+                <Text style={styles.cardText}>{pokemon.type}</Text>
+                <Text style={styles.cardText}>{pokemon.name}</Text>
+              </View>
+            );
+          })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight,
+  },
+
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    borderWidth: 1,
+  },
+
+  cardText: {
+    fontSize: 30,
   },
 });
